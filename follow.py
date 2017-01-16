a5cd0bc3b3a16770ff80f496aa83e1f66897f48f
McIntyre Watts <Recaiden@users.noreply.github.com>
2017-01-16 16:25:46 -0600

    Add files via upload
    Moved navigation logic into dot classes

--- follow.py
+++ follow.py
@@ -15,10 +15,11 @@ class POINT(Structure):
 
 SQUARE_SIZE = 3
 
+NUM_BIOTS = 10
 TPS = 60
 TICK_SPEED = 1000/TPS
 MOVEMENT_FACTOR = 2
-DISTANCE_AVERSION = 25
+DISTANCE_AVERSION = 25*25
 FOCUS_ON_GOAL = 0.05
 FOCUS_ON_COHESION = 0.05
 FOCUS_ON_AVOIDANCE = 0.07
@@ -77,9 +78,8 @@ class Board(QtGui.QFrame):
         self.timer = QtCore.QBasicTimer()
         self.pieces = []
         
-        for i in range(10):
+        for i in range(NUM_BIOTS):
             self.curPiece = Shape()
-            self.curPiece.setRandomShape()
             self.curPiece.x = random.randint(0, self.frameRect().width())
             self.curPiece.y = random.randint(0, self.frameRect().height())
             self.pieces.append(self.curPiece)
@@ -91,7 +91,7 @@ class Board(QtGui.QFrame):
 
         self.isStarted = False
         self.isPaused = False
-        
+
         self.target = QtCore.QPointF(5, 10)
         
     def start(self):
@@ -154,77 +154,27 @@ class Board(QtGui.QFrame):
             QtGui.QFrame.timerEvent(self, event)
 
     def moveTowardsTarget(self):
-        headings = [piece.heading for piece in self.pieces]
+        #headings = [piece.heading for piece in self.pieces]
         xAvg = sum([piece.x for piece in self.pieces])/len(self.pieces)
         yAvg = sum([piece.y for piece in self.pieces])/len(self.pieces)
 
-        for piece in self.pieces:
-            speedTemp = MOVEMENT_FACTOR
-            
+        for piece in self.pieces:            
             # Towards other boids
-            vectorRawX = xAvg - piece.x
-            vectorRawY = yAvg - piece.y
+            piece.navigateTowardsOthers(xAvg, yAvg)
 
-            divisorUnit = math.sqrt(vectorRawX**2 + vectorRawY**2)
-        
-            vectorX = min(max(vectorRawX/divisorUnit, -1), 1)
-            vectorY = min(max(vectorRawY/divisorUnit, -1), 1)
-
-            piece.updateHeading(vectorX, vectorY, FOCUS_ON_COHESION)
-            
             # Towards target, but don't ram it
-            vectorRawX = self.target.x() - piece.x
-            vectorRawY = self.target.y() - piece.y
-
-            divisorUnit = math.sqrt(vectorRawX**2 + vectorRawY**2)
+            piece.navigateToTarget(self.target)
             
-            vectorX = min(max(vectorRawX/divisorUnit, -1), 1)
-            vectorY = min(max(vectorRawY/divisorUnit, -1), 1)
-
-            # when you get too close, pick a direction to start turning away.
-            # Keep turning that direction until you get far enough away again
-            if piece.state is STATE_NORMAL and divisorUnit <= DISTANCE_AVERSION:
-                piece.state = random.choice([STATE_TURN_RIGHT, STATE_TURN_LEFT])
-            if piece.state is STATE_TURN_LEFT:
-                vectorY, vectorX = vectorX, -1*vectorY
-            elif piece.state is STATE_TURN_RIGHT:
-                vectorY, vectorX = -1*vectorX, vectorY
-
-            piece.updateHeading(vectorX, vectorY, FOCUS_ON_GOAL)
-
-            if divisorUnit > DISTANCE_AVERSION and piece.state is not STATE_NORMAL:
-                piece.state = STATE_NORMAL
-
             # Aversion
-            vectorXAversion = 0
-            vectorYAversion = 0
-            for pieceOther in self.pieces:
-                if pieceOther is piece:
-                    continue
-                d = piece.distanceTo(pieceOther)
-                if d > DISTANCE_AVERSION:
-                    continue
-                vectorRawX = (piece.x - pieceOther.x)
-                vectorRawY = (piece.y - pieceOther.y)
-
-                divisorUnit = math.sqrt(vectorRawX**2 + vectorRawY**2)
-        
-                vectorX = min(max(vectorRawX/divisorUnit, -1), 1)
-                vectorY = min(max(vectorRawY/divisorUnit, -1), 1)
-                
-                vectorXAversion += vectorX
-                vectorYAversion += vectorY
-            piece.updateHeading(vectorX, vectorY, FOCUS_ON_AVOIDANCE)
-            #if vectorXAversion or vectorYAversion:
-                #speedTemp *= 1.5
+            piece.navigateClear(self.pieces)
                 
             # Gather and go
             piece.finalizeHeading()
 
-            piece.x += math.cos(piece.heading) * speedTemp
-            piece.y += math.sin(piece.heading) * speedTemp
+            piece.x += math.cos(piece.heading)*MOVEMENT_FACTOR
+            piece.y += math.sin(piece.heading)*MOVEMENT_FACTOR
 
-            self.update()
+        self.update()
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
@@ -244,57 +194,39 @@ class Board(QtGui.QFrame):
                       0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
 
         color = QtGui.QColor(colorTable[shape])
-        #painter.fillRect(x + 1, y + 1, SQUARE_SIZE - 2, SQUARE_SIZE - 2, color)
 
         painter.setPen(color)
-        
+
+        # Circle with line pointing in the direction of travel and 2 shading arcs
         painter.drawEllipse(QtCore.QPoint(x, y), SQUARE_SIZE, SQUARE_SIZE)
         painter.drawLine(x, y,
                          x+math.cos(angle*math.pi/180)*SQUARE_SIZE, y+math.sin(angle*math.pi/180)*SQUARE_SIZE)
-
         painter.setPen(color.darker())
         painter.drawArc(x-SQUARE_SIZE+1, y-SQUARE_SIZE+1,
                         SQUARE_SIZE*2-2, SQUARE_SIZE*2-2,
                         (-angle+270)*16, (-180)*16)
-
         painter.setPen(color.lighter())
         painter.drawArc(x-SQUARE_SIZE+1, y-SQUARE_SIZE+1,
                         SQUARE_SIZE*2-2, SQUARE_SIZE*2-2,
                         (-angle+90)*16, (-180)*16)
-
-class Shapeforms(object):
-    
-    NoShape = 0
-    Shape = 1
-
 class Shape(object):
     
     def __init__(self):
         self.state = STATE_NORMAL
+        self.target = QtCore.QPointF(0, 0)
         self.x = 0
         self.y = 0
         self.xBuffer = 0
         self.yBuffer = 0
         self.heading = 0.0
-        self.pieceShape = Shapeforms.NoShape
+        self.pieceShape = 1
 
         self.countOfUpdateVectorsSinceFinalizing = 0
+        
+    def distanceToSquare(self, other):
+        return (other.x-self.x)**2+(other.y-self.y)**2
 
-        self.setShape(Shapeforms.NoShape)
-
-    def shape(self):
-        return self.pieceShape
-
-    def setShape(self, shape):
-        self.pieceShape = shape
-
-    def setRandomShape(self):
-        self.setShape(1)
-
-    def distanceTo(self, other):
-        return math.sqrt((other.x-self.x)**2+(other.y-self.y)**2)
-
-    def updateHeading(self, xn, yn, weight=1):
+    def updateHeading(self, xn, yn, weight=0.05):
         self.xBuffer += xn*weight
         self.yBuffer += yn*weight
 
@@ -315,6 +247,62 @@ class Shape(object):
         self.xBuffer = 0#math.cos(self.heading)
         self.yBuffer = 0#math.sin(self.heading)
 
+    def navigateToTarget(self, target):
+        vectorRawX = target.x() - self.x
+        vectorRawY = target.y() - self.y
+
+        divisorUnit = math.sqrt(vectorRawX**2 + vectorRawY**2)
+            
+        vectorX = min(max(vectorRawX/divisorUnit, -1), 1)
+        vectorY = min(max(vectorRawY/divisorUnit, -1), 1)
+
+        # when you get too close, pick a direction to start turning away.
+        # Keep turning that direction until you get far enough away again
+        if self.state is STATE_NORMAL and divisorUnit <= DISTANCE_AVERSION:
+            self.state = random.choice([STATE_TURN_RIGHT, STATE_TURN_LEFT])
+        if self.state is STATE_TURN_LEFT:
+            vectorY, vectorX = vectorX, -1*vectorY
+        elif self.state is STATE_TURN_RIGHT:
+            vectorY, vectorX = -1*vectorX, vectorY
+
+        self.updateHeading(vectorX, vectorY, FOCUS_ON_GOAL)
+
+        if divisorUnit > math.sqrt(DISTANCE_AVERSION) and self.state is not STATE_NORMAL:
+            self.state = STATE_NORMAL
+
+    def navigateTowardsOthers(self, xAvg, yAvg):
+        vectorRawX = xAvg - self.x
+        vectorRawY = yAvg - self.y
+        
+        divisorUnit = math.sqrt(vectorRawX**2 + vectorRawY**2)
+        
+        vectorX = min(max(vectorRawX/divisorUnit, -1), 1)
+        vectorY = min(max(vectorRawY/divisorUnit, -1), 1)
+        
+        self.updateHeading(vectorX, vectorY, FOCUS_ON_COHESION)
+
+    def navigateClear(self, obstacles):
+        vectorXAversion = 0
+        vectorYAversion = 0
+        vectorX = 0
+        vectorY = 0
+        for pieceOther in obstacles:
+            if pieceOther is self:
+                continue
+            d = self.distanceToSquare(pieceOther)
+            if d > DISTANCE_AVERSION:
+                continue
+            vectorRawX = (self.x - pieceOther.x)
+            vectorRawY = (self.y - pieceOther.y)
+
+            divisorUnit = math.sqrt(vectorRawX**2 + vectorRawY**2)
+            
+            vectorX = min(max(vectorRawX/divisorUnit, -1), 1)
+            vectorY = min(max(vectorRawY/divisorUnit, -1), 1)
+            
+            vectorXAversion += vectorX
+            vectorYAversion += vectorY
+        self.updateHeading(vectorX, vectorY, FOCUS_ON_AVOIDANCE)
 
 def main():
     
